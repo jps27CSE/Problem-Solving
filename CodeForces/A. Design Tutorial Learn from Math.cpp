563137e719c091315e08ee28fff6fe771250f491
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-06-26 00:14:05 +0600

    Create A. Design Tutorial Learn from Math.cpp

---
+++ CodeForces/A. Design Tutorial Learn from Math.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a; 
+    cin>>a;
+
+    if(a%2==0)
+    {
+        cout<<"4"<<" "<<a-4<<endl;
+    }
+    else
+    {
+        cout<<"9"<<" "<<a-9<<endl;
+    }
+}
