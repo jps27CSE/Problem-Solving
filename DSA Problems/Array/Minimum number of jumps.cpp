c85785943106e343b55e790422dfd2906096544c
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-07-18 20:16:14 +0600

    Update Minimum number of jumps.cpp

--- DSA Problems/Array/Minimum number of jumps.cpp
+++ DSA Problems/Array/Minimum number of jumps.cpp
@@ -1,3 +1,16 @@
+/*
+Input:
+N = 11 
+arr[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9} 
+Output: 3 
+Explanation: 
+First jump from 1st element to 2nd 
+element with value 3. Now, from here 
+we jump to 5th element with value 9, 
+and from here we will jump to the last. 
+
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
