714635080e621f86c45306900c93b312d78090b8
jps27CSE <58485174+jps27CSE@users.noreply.github.com>
2022-08-12 11:47:08 +0600

    Create Remove Element.js

---
+++ DSA Problems/Array/Remove Element.js
@@ -0,0 +1,19 @@
+/*
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+*/
+
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+
+  return nums.length;
+};
