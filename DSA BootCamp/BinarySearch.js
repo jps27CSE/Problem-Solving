36f1097293048e61e8b92339f58275419382135d
jps27CSE <58485174+jps27CSE@users.noreply.github.com>
2022-08-14 13:33:13 +0600

    accending descending binary search

--- DSA BootCamp/BinarySearch.js
+++ DSA BootCamp/BinarySearch.js
@@ -1,4 +1,4 @@
-function BinarySearch(array, target) {
+function ascendingOrderBinarySearch(array, target) {
   let start = 0;
   let end = array.length - 1;
 
@@ -16,9 +16,27 @@ function BinarySearch(array, target) {
   return "not found";
 }
 
+function descendingOrderBinarySearch(array, target) {
+  let start = 0;
+  let end = array.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor(start + (end - start) / 2);
+
+    if (target > array[mid]) {
+      end = mid - 1;
+    } else if (target < array[mid]) {
+      start = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+  return "not found";
+}
+
 let array = [1, 2, 3, 4, 5, 6, 8, 19, 23];
 let array2 = [23, 19, 8, 6, 5, 4, 3, 2, 1];
 let target = 2;
 
-console.log(BinarySearch(array, target));
-console.log(BinarySearch(array2, target));
+console.log(ascendingOrderBinarySearch(array, target));
+console.log(descendingOrderBinarySearch(array2, 19));
